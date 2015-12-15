6a1b16c71300de0ae085caa795622556fe994a04
Jan Tojnar <jtojnar@gmail.com>
2015-12-15 08:23:50 +0100

    add an option for opening the tabs in background

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cm = require('sdk/context-menu');
 const _ = require('sdk/l10n').get;
 const { getMostRecentBrowserWindow } = require('sdk/window/utils')
 const data = require("sdk/self").data;
+const prefs = require("sdk/simple-prefs").prefs;
 
 cm.Item({
 	context: cm.SelectorContext('img'),
@@ -9,8 +10,10 @@ cm.Item({
 	contentScriptFile: data.url('context-menu-action.js'),
 	onMessage: function(imageUrl) {
 		const url = 'http://www.google.com/searchbyimage?image_url=' + encodeURIComponent(imageUrl) + '&encoded_image=&image_content=&filename=&hl=&bih=&biw=';
-		const browser = getMostRecentBrowserWindow().gBrowser
-		const tab = browser.loadOneTab(url, {relatedToCurrent: true})
-		browser.selectedTab = tab;
+		const browser = getMostRecentBrowserWindow().gBrowser;
+		const tab = browser.loadOneTab(url, {relatedToCurrent: true});
+		if (!prefs.loadInBackground) {
+			browser.selectedTab = tab;
+		}
 	}
 });
