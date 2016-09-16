3769b54e7c28634ea5b7acac227abbe74df08f9f
Jan Tojnar <jtojnar@gmail.com>
2016-09-16 03:58:17 +0200

    Make tabs open adjacent to parent tabs

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const defaultPrefs = {
+	loadInBackground: true
+};
+
 browser.contextMenus.create({
 	title: browser.i18n.getMessage('context_menu_search'),
 	contexts: ['image'],
-	onclick: info => {
-		browser.storage.local.get(null, prefs => {
+	onclick: (info, tab) => {
+		browser.storage.local.get(null, currentPrefs => {
+			const prefs = Object.assign({}, defaultPrefs, currentPrefs);
 			const url = 'http://www.google.com/searchbyimage?image_url=' + encodeURIComponent(info.srcUrl) + '&encoded_image=&image_content=&filename=&hl=&bih=&biw=';
 			browser.tabs.create({
 				'active': !prefs.loadInBackground,
-				'url': url
+				'url': url,
+				'openerTabId': tab.id
 			});
 		});
 	}
